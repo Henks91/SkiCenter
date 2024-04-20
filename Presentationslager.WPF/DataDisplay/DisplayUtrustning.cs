2aa399fec621e8611b8d4d631e5b473ec10f2008
Henks91 <98890612+Henks91@users.noreply.github.com>
2024-04-20 16:34:51 +0200

    Add files via upload

---
+++ Presentationslager.WPF/DataDisplay/DisplayUtrustning.cs
@@ -0,0 +1,27 @@
+﻿using Entiteter.Tjänster;
+
+namespace PresentationslagerWPF.DataDisplay
+{
+    public class DisplayUtrustning
+    {
+        public DisplayUtrustning(int antal, Utrustning propUtrustning, string typ, string benämning, int summa)
+        {
+            Value = antal;
+            PropUtrustning = propUtrustning;
+            Typ = typ;
+            Benämning = benämning;
+            Summa = summa;
+        }
+
+        public int Value { get; set; }
+
+        public Utrustning PropUtrustning { get; set; }
+
+        public string Typ { get; set; }
+
+        public string Benämning { get; set; }
+
+        public int Summa { get; set; }
+
+    }
+}
