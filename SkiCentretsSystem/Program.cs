a8d73e71556454bd6f3db25accfe43ad2f2bbcce
Henks91 <98890612+Henks91@users.noreply.github.com>
2024-04-20 16:21:54 +0200

    Add files via upload

---
+++ SkiCentretsSystem/Program.cs
@@ -0,0 +1,21 @@
+﻿using Affärslager;
+
+
+
+
+internal class Program
+{
+
+
+    public static void Main(string[] args)
+    {
+
+        BokningsKontroller kontroller = new BokningsKontroller();
+        kontroller.LaddaData();
+
+        PrisKontroller prisKontroller= new PrisKontroller();
+
+        
+        prisKontroller.BeräknaPrisLogi();
+    }
+}
